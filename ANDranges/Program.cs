55e9c843aeb9af0f0f16c2a799e434823cec11af
KKalamees <karmo.kalamees@gmail.com>
2024-09-27 15:35:33 +0300

    add week 5 code

---
+++ ANDranges/Program.cs
@@ -0,0 +1,28 @@
+﻿
+Console.WriteLine("Enter the temperature: ");
+int temp = Int32.Parse(Console.ReadLine());
+
+if(temp <= 0)
+{
+    Console.WriteLine("Freezing cold");
+}
+else if(temp > 0 &&  temp <= 10)
+{
+    Console.WriteLine("Cold");
+}
+else if(temp > 10 && temp <= 15)
+{
+    Console.WriteLine("Chilly");
+}
+else if (temp > 15 && temp < 20)
+{
+    Console.WriteLine("Warm");
+}
+else if (temp >= 20 && temp < 30)
+{
+    Console.WriteLine("Hot");
+}
+else
+{
+    Console.WriteLine("Boiling hot");
+}
